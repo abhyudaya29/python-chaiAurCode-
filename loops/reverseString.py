577d327827b8a74b2ad9ed1836515583bdea89b7
abhyudaya29 <abhyudayadev123@gmail.com>
2024-04-08 22:57:15 +0530

    reverseString

--- loops/reverseString.py
+++ loops/reverseString.py
@@ -1,4 +1,23 @@
 character="chai"
-
+reverse_char=""
 for i in character:
-    
+    reverse_char=i+reverse_char
+print(reverse_char)
+"""
+character="chai"
+new=""
+reverse=""
+for i in char:(i character ki hr ek value or iterate krega ):
+       new=     new+i
+       1)new=""+c=c
+       2)new="c"+h=ch
+       3)new="ch"+a=cha
+       4)new="cha"+i=chai
+for reversing 
+reverse=i+reverse
+1)reverse=c+""=c
+2)reverse=h+"c"=hc
+3)reverse=a+"hc"=ach
+4)reverse=i+"ahc"=iahc
+
+"""
