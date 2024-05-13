a5d0bcbcec5f62b8aed90799bb29a5b1e1d3d502
abhyudaya29 <abhyudayadev123@gmail.com>
2024-05-13 23:51:56 +0530

    lambda funtion

---
+++ functions/functions6.py
@@ -0,0 +1,9 @@
+def add(a,b):
+    return a+b
+
+result =add(1,2)
+
+# using lambda function
+# lambda generally used when the usage of function is only limited
+cube=lambda x:x*3
+print(cube(3),"Cube of number using lambda")
