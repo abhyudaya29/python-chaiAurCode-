8e4764086dcbbd02beb930d51116bf87671e87e7
abhyudaya29 <abhyudayadev123@gmail.com>
2024-05-13 23:51:42 +0530

    greet functions

---
+++ functions/functions5.py
@@ -0,0 +1,5 @@
+def greet_Message(name="user"):
+    return "Hellow, "+name+"!"
+
+print(greet_Message(),"Greeting with default message")
+print(greet_Message("Chai"),"Greeting with passed  parameter")
